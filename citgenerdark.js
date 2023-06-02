e590ae9f167e164804869229fc722ed78a08428e
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-06-02 19:48:34 +0600

    Update citgenerdark.js

--- citgenerdark.js
+++ citgenerdark.js
@@ -1,21 +1,9 @@
-jQuery(document).ready(function($){
-// автообновление 10сек
-// setTimeout(function(){
-//     location.reload();
-// }, 5000);
-
-
-// btn1.onclick = function() {
-//     // if (this.innerHTML=='ON') this.innerHTML = 'OFF';
-//     // else this.innerHTML = 'ON';
-//     // return false;
-// }
 const text = document.getElementById("text");
 const button = document.getElementById("btn1");
 
-button.addEventListener("click", function() {
-  text.textContent = "Новый текстe";
-});
+// button.addEventListener("click", function() {
+//   text.textContent = "Новый текстe";
+// });
 button.addEventListener("click", function() {
   fetch('citatadark.txt')
     .then(response => response.text())
@@ -60,6 +48,3 @@ button.addEventListener("click", function() {
 // const btn1 = document.getElementById('btn1');
 // btn1.addEventListener('click', getRandomString);
   
-
-
-// });
