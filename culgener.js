f2a9f5a44f342f467c84e62f6f2eb24ef74a847b
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-06-30 20:09:18 +0600

    Update culgener.js

--- culgener.js
+++ culgener.js
@@ -5,12 +5,16 @@ wordInput.addEventListener('input', function() {
   const word = wordInput.value.toLowerCase();
   let output = '';
 
-  if (word === 'привет') {
-    output = 'hello';
-  } else if (word === 'салам') {
-    output = 'салам';
-  } else if (word === 'hi') {
-    output = 'hi';
+  if (word === 'хуй' || word === 'член') {
+    output = 'Хер, банан, петух';
+  } else if (word === 'блять' || word === 'сука') {
+    output = 'Блин, ой, вот это да';
+  } else if (word === 'ебать' || word === 'охуенно') {
+    output = 'Здорово! Невероятно, очень хорошо';
+  } else if (word === 'пиздец' || word === 'ебанный рот') {
+    output = 'Эх, не повезло, черт, все очень плохо.';
+  } else if (word === 'иди нахуй' || word === 'заебал') {
+    output = 'отстань, надоел, уйди отсюда';
   } else {
     output = 'Нет соответствующих слов';
   }
