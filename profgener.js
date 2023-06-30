6a76afbbafb13d24ac9f59c5e66ac3295c717e27
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-06-30 10:53:29 +0600

    Create profgener.js

---
+++ profgener.js
@@ -0,0 +1,12 @@
+const text = document.getElementById("text");
+const button = document.getElementById("btn1");
+
+button.addEventListener("click", function() {
+  fetch('prof.txt')
+    .then(response => response.text())
+    .then(data => {
+      const lines = data.split('\n');
+      const randomIndex = Math.floor(Math.random() * lines.length);
+      text.textContent = lines[randomIndex];
+    });
+});
