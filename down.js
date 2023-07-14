56022f71d681f3de8ab2ce099928d8fa5e020e13
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-14 20:20:37 +0600

    Create down.js

---
+++ down.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('down');
+const causeplcDiv = document.querySelector('.causeplc');
+
+
+button.addEventListener('click', function() {
+  causeplcDiv.style.height = '450px';
+});
