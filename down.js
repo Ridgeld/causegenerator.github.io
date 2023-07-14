beb0039f061869523bc3026d4f9267319d97a672
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-14 20:32:16 +0600

    Update down.js

--- down.js
+++ down.js
@@ -3,5 +3,17 @@ const causeplcDiv = document.querySelector('.causeplc');
 
 
 button.addEventListener('click', function() {
+  const fileName = 'prof.png';
+  const filePath = 'image/prof.png';
+  const element = document.createElement('a');
+  element.href = filePath;
+  element.download = fileName;
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+  
   causeplcDiv.style.height = '450px';
 });
