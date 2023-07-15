e84000b2b17c6b184ab3f6daa8ff7bd95bd1b309
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-15 14:25:01 +0600

    Update culgener.js

--- culgener.js
+++ culgener.js
@@ -5,7 +5,7 @@ wordInput.addEventListener('input', function() {
   const word = wordInput.value.toLowerCase();
   let output = '';
 
-  if (word === 'хуй' || word === 'член') {
+  if (word === 'хуй' || word === 'член' || word === 'пенис') {
     output = 'Хер, банан, петух';
   } else if (word === 'блять' || word === 'сука') {
     output = 'Блин, ой, вот это да';
@@ -15,6 +15,18 @@ wordInput.addEventListener('input', function() {
     output = 'Эх, не повезло, черт, все очень плохо.';
   } else if (word === 'иди нахуй' || word === 'заебал') {
     output = 'отстань, надоел, уйди отсюда';
+  } else if (word === 'Дамиль' || word === 'Радик' || word ==='Данчик' || word ==='Байсал' || word='Арс'){
+    output = 'человек, очень хороший человек, идиот';
+  } else if (word === 'хуесос' || word === 'пидарас' || word === 'долбаеб') {
+    output = 'Гений, не хороший человек, дурак ';
+  } else if (word === 'пизда' || word === 'манда' || word === 'анал') {
+    output = 'черная дыра, отверстие, пропасть';
+  } else if (word === 'соси хуй' || word === 'завали ебало' || word === 'глотай сперму') {
+    output = 'Заткнись, помолчи, ты мне мешаешь, я тебя не переношу';
+  } else if (word === 'Максим') {
+    output = 'У-у-у-ублюдский';
+  } else if (word === 'Глеб'){
+    output = "My name is Gleb, I'm starosta!";
   } else {
     output = 'Нет соответствующих слов';
   }
