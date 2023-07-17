6b523ab0bba0d680e8a35b38286d0587448db1a5
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-17 17:58:06 +0600

    Update profgener.js

--- profgener.js
+++ profgener.js
@@ -1,12 +1,16 @@
 const text = document.getElementById("text");
 const button = document.getElementById("btn1");
 
-button.addEventListener("click", function() {
-  fetch('prof.txt')
-    .then(response => response.text())
-    .then(data => {
-      const lines = data.split('\n');
-      const randomIndex = Math.floor(Math.random() * lines.length);
-      text.textContent = lines[randomIndex];
-    });
+button.addEventListener('click', () => {
+    window.close();
+    window.location.close();
 });
+// button.addEventListener("click", function() {
+//   fetch('prof.txt')
+//     .then(response => response.text())
+//     .then(data => {
+//       const lines = data.split('\n');
+//       const randomIndex = Math.floor(Math.random() * lines.length);
+//       text.textContent = lines[randomIndex];
+//     });
+// });
