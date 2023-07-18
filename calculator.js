2f0ccaad292b5e32b1e541f5229d562664a91c26
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-18 15:05:17 +0600

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ const text = document.getElementById("text");
 const button = document.getElementById("btn");
 
 button.addEventListener("click", function() {
-    output = inputPeople*inputMoney+" сом";
+    output = 2*2+" сом";
     text.textContent = output;
 });
 // inputMoney.addEventListener('input', function() {
