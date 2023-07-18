d7c6632b7644f1b3966f9f21eab3166ca3135d74
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-18 17:17:20 +0600

    Update download.js

--- download.js
+++ download.js
@@ -4,6 +4,18 @@ const downSec = document.getElementById('downSec');
 
 
 downFir.addEventListener('click', function() {
+    const fileName = 'AdvancedKey.pdf';
+    const filePath = 'files/AdvancedKey.pdf';
+    const element = document.createElement('a');
+    element.href = filePath;
+    element.download = fileName;
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+});
+
+downSec.addEventListener('click', function() {
     const fileName = 'UpperKey.rar';
     const filePath = 'files/UpperKey.rar';
     const element = document.createElement('a');
