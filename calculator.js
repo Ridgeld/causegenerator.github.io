3d82598b7843c03aa75c23977ab95e5a2b04f8e0
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-18 16:32:17 +0600

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,8 +5,8 @@ const text = document.getElementById("text");
 const button = document.getElementById("btn");
 
 inputMoney.addEventListener('input', () => {
-    const procentValue = parseFloat(inputProcent.value);
-    const moneyValue = parseFloat(inputMoney.value);
-    const result = (moneyValue*procentValue) +' сом';
+    const procentValue = +parseFloat(inputProcent.value);
+    const moneyValue = +parseFloat(inputMoney.value);
+    const result = (moneyValue*(procentValue/100)) +' сом';
     text.textContent = result;
 });
