f33ae8146792501fc44c6af61a9e30f1b0bbd398
Daniel Mustafin <daniel.mustafin076@gmail.com>
2023-08-19 13:07:02 +0600

    testing

---
+++ update.js
@@ -0,0 +1,25 @@
+fetch('update.json')
+.then(response => response.json())
+.then(data => {
+  const updatesContainer = document.getElementById('container'); // контейнер для обновлений
+
+  // Проходимся по каждому элементу в массиве данных
+  data.forEach(update => {
+    const updateDiv = document.createElement('div');
+    updateDiv.className = 'updatePlace'; // добавляем класс update
+
+    const dateDiv = document.createElement('div');
+    dateDiv.className = 'date';
+    dateDiv.textContent = update.data;
+
+    const textDiv = document.createElement('div');
+    textDiv.className = 'text';
+    textDiv.textContent = update.text;
+
+    updateDiv.appendChild(dateDiv);
+    updateDiv.appendChild(textDiv);
+
+    updatesContainer.appendChild(updateDiv);
+  });
+})
+.catch(error => console.error('Error:', error));
