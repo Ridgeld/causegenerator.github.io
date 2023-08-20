1a45d81ebabe29970e925b7b1a9d7cac3fbcac86
Daniel Mustafin <daniel.mustafin076@gmail.com>
2023-08-20 10:08:37 +0600

    131111

---
+++ doodle.js
@@ -0,0 +1,24 @@
+let board;
+let boardWidth = 350;
+let boardHeight = 550;
+let context;
+
+document.body.style.overflow = 'hidden';
+
+const scoreCount = document.getElementById('score');
+const notifName = document.getElementById('notifName');
+const text = document.getElementById('text');
+const text2 = document.getElementById('text2');
+
+
+const overlay = document.getElementById('overlay');
+const notification = document.getElementById('notification');
+const finishButton = document.getElementById('finish');
+const continueButton = document.getElementById('continue');
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d");
+}
