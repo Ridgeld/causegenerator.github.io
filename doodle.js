eea95dc8817d7cf870b879c9eaa338595373d119
Daniel Mustafin <daniel.mustafin076@gmail.com>
2023-08-20 10:21:18 +0600

    werqq

--- doodle.js
+++ doodle.js
@@ -16,9 +16,32 @@ const notification = document.getElementById('notification');
 const finishButton = document.getElementById('finish');
 const continueButton = document.getElementById('continue');
 
+
+//doodler
+
+let doodlerWidth = 46;
+let doodlerHeight = 46;
+let doodlerX = boardWidth/2 - doodlerWidth/2;
+let doodlerY = boardHeight*7/8 - doodlerHeight;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+let doodler = {
+    img: null,
+    x: doodlerX,
+    y: doodlerY,
+    width: doodlerWidth,
+    height: doodlerHeight
+}
+
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");
+
+
+    context.fillStyle = "green";
+    context.fillRect(doodler.x, doodler.y, doodler.width, doodler.height);
 }
