c4ea6041e43fe5332fe317d6d38944c2b48689c1
Daniel Mustafin <daniel.mustafin076@gmail.com>
2023-09-12 22:13:47 +0600

    add new functions

--- download.js
+++ download.js
@@ -4,15 +4,28 @@ const downSec = document.getElementById('downSec');
 
 
 downFir.addEventListener('click', function() {
-    const fileName = 'AdvancedKey.pdf';
-    const filePath = 'files/AdvancedKey.pdf';
-    const element = document.createElement('a');
-    element.href = filePath;
-    element.download = fileName;
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+    const downloadNotof = () =>{
+        notifName.textContent = 'Скачать';
+        text2.textContent = "";
+        text.textContent = "Вы хотите скачать Solutions Advanced?";
+        overlay.classList.add('show');
+        notification.classList.add('show');
+    }
+    finishButton.addEventListener('click', function() {
+        const fileName = 'AdvancedKey.pdf';
+        const filePath = 'files/AdvancedKey.pdf';
+        const element = document.createElement('a');
+        element.href = filePath;
+        element.download = fileName;
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    });
+    
+    continueButton.addEventListener('click', function() {
+        location.reload();
+    });
 });
 
 downSec.addEventListener('click', function() {
