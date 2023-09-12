3ebaeef077b1ecee8b186c0b40d64511937221eb
Daniel Mustafin <daniel.mustafin076@gmail.com>
2023-09-12 23:07:52 +0600

    correct

--- download.js
+++ download.js
@@ -30,27 +30,32 @@ continueButton.addEventListener('click', function() {
     document.body.removeChild(element);
 });
 
+
 downSec.addEventListener('click', function() {
     notifName.textContent = 'Скачать';
     text2.textContent = "";
     text.textContent = "Вы хотите скачать Solutions Upper-Intermediate?";
     overlay.classList.add('show');
     notification.classList.add('show');
+    let second = AdvancedButton;
 });
-finishButton.addEventListener('click', function() {
-    location.reload();
-});
-continueButton.addEventListener('click', function() {
-    const fileName = 'UpperKey.rar';
-    const filePath = 'files/UpperKey.rar';
-    const element = document.createElement('a');
-    element.href = filePath;
-    element.download = fileName;
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-});
+function AdvancedButton(){
+    finishButton.addEventListener('click', function() {
+        location.reload();
+    });
+    continueButton.addEventListener('click', function() {
+        const fileName = 'UpperKey.rar';
+        const filePath = 'files/UpperKey.rar';
+        const element = document.createElement('a');
+        element.href = filePath;
+        element.download = fileName;
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    });
+
+}
 downThr.addEventListener('click', function() {
     const fileName = 'MegaSetUp.exe';
     const filePath = 'files/MegaSetUp.exe';
