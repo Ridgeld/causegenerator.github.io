2d4efeb1c9eed16c0b79cb51538fc5e2aa1c40e6
Daniel <rayl.alim.2014@gmail.com>
2023-10-16 22:42:56 +0600

    new function

--- citata.js
+++ citata.js
@@ -1,12 +1,39 @@
-const text = document.getElementById("text");
-const button = document.getElementById("btn");
+// const text = document.getElementById("text");
+// const button = document.getElementById("btn");
 
-button.addEventListener("click", function() {
-  fetch('assets/citata.txt')
+//   fetch('assets/citata.txt')
+//     .then(response => response.text())
+//     .then(data => {
+//       const lines = data.split('\n');
+//       const randomIndex = Math.floor(Math.random() * lines.length);
+//       text.textContent = lines[randomIndex];
+//     });
+// });
+
+let usedTexts = [];
+let lines;
+
+function showRandomText() {
+    if (usedTexts.length === lines.length) {
+        location.reload();
+        return;
+    }
+
+    let randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * lines.length);
+    } while (usedTexts.includes(randomIndex));
+
+    usedTexts.push(randomIndex);
+    const randomText = lines[randomIndex];
+
+    const text = document.getElementById("text");
+    text.textContent = randomText;
+}
+
+fetch("assets/citata.txt")
     .then(response => response.text())
-    .then(data => {
-      const lines = data.split('\n');
-      const randomIndex = Math.floor(Math.random() * lines.length);
-      text.textContent = lines[randomIndex];
+    .then(text => {
+        lines = text.split('\n');
+        document.querySelector("#button").addEventListener("click", showRandomText);
     });
-});
